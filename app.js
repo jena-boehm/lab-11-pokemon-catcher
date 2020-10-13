6a8927b8d3d104e71320d2d8bdade87d98df2e98
Jena Boehm <boehmjena@gmail.com>
2020-10-12 21:39:34 -0700

    Add refresh pokemon function and button event listener

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions and grab DOM elements
-import { rawPokemonData } from './pokemon-data.js';
+import { refreshPokemon } from './utils.js';
 
 const radios = document.querySelectorAll('input');
 const catchButton = document.querySelector('button');
@@ -10,37 +10,21 @@ const images = document.querySelectorAll('label > img');
 // initialize state
 let totalRounds = 0;
 let pokemonCaptured = 0;
+let pokemonCapturedArray = 0;
+let pokemonEncountered = 0;
+let pokemonEncounteredArray = [];
 
-
-function getRandomPokemon(array) {
-    const index = Math.floor(Math.random() * array.length);
-
-    return array[index];
-}
-
-let optionOne = getRandomPokemon(rawPokemonData);
-let optionTwo = getRandomPokemon(rawPokemonData);
-let optionThree = getRandomPokemon(rawPokemonData);
-
-while (optionOne.id === optionTwo.id) {
-    optionOne = getRandomPokemon(rawPokemonData);
-}
-while (optionTwo.id === optionThree.id) {
-    optionTwo = getRandomPokemon(rawPokemonData);
-}
-while (optionThree.id === optionOne.id) {
-    optionThree = getRandomPokemon(rawPokemonData);
-}
-
-console.log(optionOne, optionTwo, optionThree);
-
-radios[0].value = optionOne.id;
-images[0].src = optionOne.url_image;
-
-radios[1].value = optionTwo.id;
-images[1].src = optionTwo.url_image;
-
-radios[2].value = optionThree.id;
-images[2].src = optionThree.url_image;
 
 // set event listeners to update state and DOM
+
+catchButton.addEventListener('click', () => {
+    totalRounds++;
+    pokemonEncountered++;
+    refreshPokemon();
+
+    if (totalRounds === 10) {
+        location.href = './results';
+    }
+});
+
+refreshPokemon();
---
+++ utils.js
@@ -0,0 +1,39 @@
+import { rawPokemonData } from './pokemon-data.js';
+
+const radios = document.querySelectorAll('input');
+const catchButton = document.querySelector('button');
+const rightOrWrong = document.querySelector('#right-or-wrong');
+const results = document.querySelector('#results');
+const images = document.querySelectorAll('label > img');
+
+export function getRandomPokemon(array) {
+    const index = Math.floor(Math.random() * array.length);
+
+    return array[index];
+}
+
+export function refreshPokemon() {
+    let optionOne = getRandomPokemon(rawPokemonData);
+    let optionTwo = getRandomPokemon(rawPokemonData);
+    let optionThree = getRandomPokemon(rawPokemonData);
+    
+    while (optionOne.id === optionTwo.id) {
+        optionOne = getRandomPokemon(rawPokemonData);
+    }
+    while (optionTwo.id === optionThree.id) {
+        optionTwo = getRandomPokemon(rawPokemonData);
+    }
+    while (optionThree.id === optionOne.id) {
+        optionThree = getRandomPokemon(rawPokemonData);
+    }
+    
+    
+    radios[0].value = optionOne.id;
+    images[0].src = optionOne.url_image;
+    
+    radios[1].value = optionTwo.id;
+    images[1].src = optionTwo.url_image;
+    
+    radios[2].value = optionThree.id;
+    images[2].src = optionThree.url_image;
+}
